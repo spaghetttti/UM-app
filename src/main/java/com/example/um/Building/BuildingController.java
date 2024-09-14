c868ac22ae0a64412185dab75260657916639abb
asil <asil9802mum@gmail.com>
2024-09-14 22:16:01 +0200

    testing ssh connection

--- src/main/java/com/example/um/Building/BuildingController.java
+++ src/main/java/com/example/um/Building/BuildingController.java
@@ -1,13 +1,8 @@
 package com.example.um.Building;
 
-import com.example.um.Campus.Campus;
-import com.example.um.Campus.CampusService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/buildings")
